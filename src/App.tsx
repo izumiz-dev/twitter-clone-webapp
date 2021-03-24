6c2dc8fc42794c6b443dee64bd0015dee34d155c
Izumi Zama <izumiz@izumiz.me>
2021-03-25 01:16:01 +0900

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import "./App.css";
+import { Users } from "./components/User";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <Users />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
